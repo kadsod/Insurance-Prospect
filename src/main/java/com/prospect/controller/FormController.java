169dcec5d56ea90822dc5cfbc6c93f4064d8da44
sNova29 <ksa.supernova@gmail.com>
2019-07-21 22:40:01 +0530

    Added get call to show data on grid

--- src/main/java/com/prospect/controller/FormController.java
+++ src/main/java/com/prospect/controller/FormController.java
@@ -1,9 +1,13 @@
 package com.prospect.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +18,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.prospect.business.WolframData;
 import com.prospect.model.Form;
+import com.prospect.model.Metrics;
+import com.prospect.repository.MetricsRepository;
 
 /*@CrossOrigin(origins = "https://insurance-prospect-ui.cfapps.io/", maxAge = 3600)*/
 @RestController
@@ -24,7 +30,7 @@ public class FormController {
 	WolframData data;
 	
 	@Autowired
-	
+	MetricsRepository repository;
 	
 	@RequestMapping(value= "insurance-prospect-ui.cfapps.io/**", method=RequestMethod.OPTIONS)
 	public void corsHeaders(HttpServletResponse response) {
@@ -49,4 +55,15 @@ public class FormController {
 		formObj.setResponse(resp);
 		return formObj;
 	}
+	
+	
+	
+	@CrossOrigin
+	@GetMapping(path= "/getAllUsers" , produces = "application/json")
+	
+	public ResponseEntity<List<Metrics>> getAllUsers() {
+		List<Metrics> users = repository.findAll();
+		return ResponseEntity.ok().header("total", Integer.toString(users.size())).body(users);
+	}
+
 }
